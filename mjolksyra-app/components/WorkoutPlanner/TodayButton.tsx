1957fd8df5bc00778879a3d48f34e033fffd1eb5
Per <per.sonberg@gmail.com>
2025-01-07 09:23:13 +0100

    Update TodayButton.tsx

--- mjolksyra-app/components/WorkoutPlanner/TodayButton.tsx
+++ mjolksyra-app/components/WorkoutPlanner/TodayButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CalendarHeartIcon } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 
 export function TodayButton() {
@@ -21,30 +21,31 @@ export function TodayButton() {
 
     return () => elements.forEach((el) => observer.unobserve(el));
   }, []);
+  return useMemo(() => {
+    if (isVisible) {
+      return;
+    }
 
-  if (isVisible) {
-    return;
-  }
-
-  return (
-    <Tooltip>
-      <TooltipTrigger asChild>
-        <div
-          onClick={() => {
-            document
-              .querySelectorAll('[data-today="true"]')
-              .forEach((el) =>
-                el.scrollIntoView({ behavior: "smooth", block: "center" })
-              );
-          }}
-          className="absolute text-white text-sm right-8 bottom-8 bg-red-800 hover:bg-red-700  rounded-full font-bold h-10 w-10 grid place-items-center cursor-pointer"
-        >
-          <CalendarHeartIcon className="h-4" />
-        </div>
-      </TooltipTrigger>
-      <TooltipContent>
-        <p>Back to current week</p>
-      </TooltipContent>
-    </Tooltip>
-  );
+    return (
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <div
+            onClick={() => {
+              document
+                .querySelectorAll('[data-today="true"]')
+                .forEach((el) =>
+                  el.scrollIntoView({ behavior: "smooth", block: "center" })
+                );
+            }}
+            className="absolute text-white text-sm right-8 bottom-8 bg-red-800 hover:bg-red-700  rounded-full font-bold h-10 w-10 grid place-items-center cursor-pointer"
+          >
+            <CalendarHeartIcon className="h-4" />
+          </div>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Back to current week</p>
+        </TooltipContent>
+      </Tooltip>
+    );
+  }, [isVisible]);
 }
