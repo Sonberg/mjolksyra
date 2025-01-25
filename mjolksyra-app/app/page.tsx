431dc15b29c9edd8336a8d80a9143c2ee0f28e00
Per <per.sonberg@gmail.com>
2025-01-25 18:00:24 +0100

    Update page.tsx

--- mjolksyra-app/app/page.tsx
+++ mjolksyra-app/app/page.tsx
@@ -8,9 +8,53 @@ import { WorkoutPlannerDemo } from "@/components/WorkoutPlannerDemo/WorkoutPlann
 import { isBeta } from "@/constants/isBeta";
 import { RegisterDialog } from "@/dialogs/RegisterDialog";
 import { useValidation } from "@/hooks/useValidation";
+import { DotIcon } from "lucide-react";
 import { useState } from "react";
 import { z } from "zod";
 
+type Point = {
+  title: string;
+  text?: string;
+};
+
+const points: Point[] = [
+  {
+    title: "For lifters, by lifters 🏋️‍♂️",
+    text: "Built by passionate athletes who understand your needs.",
+  },
+  {
+    title: "Extensive exercise library 📚",
+    text: "Access over 800 exercises with detailed instructions.",
+  },
+  {
+    title: "Add your own exercises ✍️",
+    text: "Customize your training by adding personal exercises.",
+  },
+  {
+    title: "Build and reuse workout blocks 🔄",
+    text: "Save time by creating reusable training components.",
+  },
+  {
+    title: "Drag-and-drop workout creation 🧩",
+    text: "Easily design your workouts with our intuitive interface.",
+  },
+  {
+    title: "Earn extra income weekly 💰",
+    text: "Secure payments powered by Stripe, directly to your account.",
+  },
+  {
+    title: "No upfront costs 🚀",
+    text: "We only take 10% per transaction—no hidden fees.",
+  },
+  {
+    title: "Unlimited athletes 🏃‍♂️",
+    text: "Coach as many athletes as you want, with no restrictions.",
+  },
+  {
+    title: "Track progress with media 📸",
+    text: "Athletes can view workouts, upload videos, images, and log results.",
+  },
+];
 export default function Home() {
   return (
     <div className="overflow-y-auto">
@@ -22,7 +66,7 @@ export default function Home() {
               for everyone.
             </h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-              Get started with our coaching journey, create and sell workout
+              Get started with your coaching journey, create and sell workout
               today. Side-hustle for PT&apos;s, Powerlifters, Crossfitters or
               Weightlifters.
             </p>
@@ -59,10 +103,26 @@ export default function Home() {
           Calculator
         </div>
       </section> */}
-      <section className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-44">
+
+      <section className="max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:pt-44 ">
+        <h2 className="text-4xl leading-relaxed font-extrabold tracking-tight pb-8">
+          Join us today!
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
+          {points.map((x) => (
+            <div key={x.title} className="py-6 md:py-8 flex gap-4 items-center">
+              <div className="grid gap-2">
+                <div className="font-bold">{x.title}</div>
+                {x.text ? <div className="text-sm">{x.text}</div> : null}
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className=" max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-40 hidden md:grid">
         <div className="mr-auto place-self-center lg:col-span-7">
           <h2 className="text-4xl leading-relaxed font-extrabold tracking-tight pb-8">
-            Try it out!
+            Try our planner!
           </h2>
         </div>
         <div className="border mr-auto place-self-center lg:col-span-12 rounded shadow-xl pl-2">
@@ -103,20 +163,23 @@ const SignupForm = () => {
   return (
     <>
       <div className="w-full flex flex-col items-start gap-4">
-        <div className="text-sm">
-          Want to stay in touch and informed when we are lunching?
-        </div>
+        <div className="text-sm">Want to stay in touch?</div>
 
         <Input
           value={email}
           onChange={(ev) => setEmail(ev.target.value)}
           placeholder="You email"
         />
-        <Button disabled={isSubmitted} onClick={onSubmit} size="sm">
+        <button
+          className="bg-accent-foreground text-accent rounded-full px-6 py-4 hover:bg-accent-foreground/90 font-semibold"
+          disabled={isSubmitted}
+          onClick={onSubmit}
+        >
           {isLoading ? <Spinner size={8} /> : null}
-          {isSubmitted ? "Thank you!" : "Keep me up-to-date"}
-        </Button>
+          {isSubmitted ? "Thank you!" : "Sign me up"}
+        </button>
       </div>
     </>
   );
 };
+ 
